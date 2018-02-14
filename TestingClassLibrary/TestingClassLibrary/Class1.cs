0fc35976535c4addc797ea9f4a2138cdcff16de2
Mark Molloy <markmolloy57@gmail.com>
2018-02-14 12:54:11 +0000

    first test

--- TestingClassLibrary/TestingClassLibrary/Class1.cs
+++ TestingClassLibrary/TestingClassLibrary/Class1.cs
@@ -10,42 +10,42 @@ namespace TestingClassLibrary
     {
         public float CalcPremium(int age, string gender)
         {
-            float premium;
+            float premium = 0f;
 
             if (gender == "female")
             {
                 if ((age >= 18)&& (age <= 30))
                 {
-                    premium = 5.0;
+                    premium = 5.0f;
                 }
                 else if(age >= 31)
                 {
-                    premium = 2.50;
+                    premium = 2.50f;
                 }
                 else
                 {
-                    premium = 0.0;
+                    premium = 0.0f;
                 }
             }
             else if (gender == "male")
             {
-                if ((age > = 18) && (age <= 35))
+                if ((age >= 18) && (age <= 35))
                 {
-                    premium = 6.0;
+                    premium = 6.0f;
                 }
                 else if (age >= 36)
                 {
-                    premium = 5.0;
+                    premium = 5.0f;
                 }
                 else
                 {
-                    premium = 0.0;
+                    premium = 0.0f;
                 }
             }
 
             if (age >= 50)
             {
-                premium = premium * 0.15;
+                //premium = premium * 0.15f;
             }
 
             return premium;
---
+++ TestingClassLibrary/TestingClassLibraryTests/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestingClassLibrary;
+
+namespace TestingClassLibraryTests
+{
+    [TestFixture]
+
+    public class Class
+    {
+        [Test]
+        public void ShouldReturnZero()
+        {
+            //age is out of range
+            Class1 sut = new Class1();
+
+            float expectedResult = sut.CalcPremium(17, "female");
+
+            Assert.That(expectedResult, Is.EqualTo(0.0));
+        }
+    }
+}
